0e797cbc19d74d6c12822d49d1999813a30987ad
David Marro <davidmarro@mbp-di-david.homenet.telecomitalia.it>
2019-08-07 16:55:32 +0200

    secondo commit

--- ToDoApp/toDoListViewController.swift
+++ ToDoApp/toDoListViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ToDoListViewController: UIViewController {
+class toDoListViewController: UITableViewController {
 
+    let itemArray = ["buy eggs","go to the Supermarket","buy headphones"]
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view.
     }
+// MARK - Table view data source
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        if(tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
 
