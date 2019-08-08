7b252d1df176b88c8c4422e5a1a94460b455664b
David Marro <davidmarro@mbp-di-david.homenet.telecomitalia.it>
2019-08-08 12:14:05 +0200

    aggiunto item model

---
+++ ToDoApp/Models/Item.swift
@@ -0,0 +1,20 @@
+//
+//  Item.swift
+//  ToDoApp
+//
+//  Created by David Marro on 08/08/2019.
+//  Copyright © 2019 David Marro. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Item {
+    var title : String
+    var done : Bool
+    init()
+    {
+        title = ""
+        done = false
+    }
+}
